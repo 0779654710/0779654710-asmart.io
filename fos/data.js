b41dabeb7b9e33690014a90e4420239f00c96bd6
0788178984 <asiimweivanlucky1@gmail.com>
2025-05-19 16:10:43 +0300

    Initial commit

---
+++ fos/data.js
@@ -0,0 +1,125 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20250519_095739_2",
+      "name": "PANO_20250519_095739_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7042099284170806,
+          "pitch": -0.009121365449596297,
+          "rotation": 0,
+          "target": "1-pano_20250310_144901_0"
+        },
+        {
+          "yaw": 2.9707330396856113,
+          "pitch": -0.011833774909414174,
+          "rotation": 0,
+          "target": "0-pano_20250519_095739_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7944002612612664,
+          "pitch": 0.07988837363405388,
+          "title": "NoticeBoard",
+          "text": "FOS"
+        },
+        {
+          "yaw": -2.718460671900594,
+          "pitch": -0.1052701761447139,
+          "title": "Enviroment",
+          "text": "Conducieve Atmosphere"
+        }
+      ]
+    },
+    {
+      "id": "1-pano_20250310_144901_0",
+      "name": "PANO_20250310_144901_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6286883879154033,
+        "pitch": 0.24864719232213517,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11515440312173908,
+          "pitch": 0.22333508259786328,
+          "rotation": 0,
+          "target": "0-pano_20250519_095739_2"
+        },
+        {
+          "yaw": 1.968340227225064,
+          "pitch": 0.03764850993549729,
+          "rotation": 0,
+          "target": "0-pano_20250519_095739_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2787081929176818,
+          "pitch": -0.017854458168665488,
+          "title": "Lab2",
+          "text": "Old Computer Lab"
+        },
+        {
+          "yaw": -2.5449130860453035,
+          "pitch": 0.07899582986597231,
+          "title": "Staff",
+          "text": "Mr. Opio"
+        }
+      ]
+    }
+  ],
+  "name": "FacultyOfScience",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
