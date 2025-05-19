0c313009a34e7de9c208afae95b6db37f68e55ea
0788178984 <asiimweivanlucky1@gmail.com>
2025-05-19 19:39:00 +0300

    Describe your update here

---
+++ educ1/data.js
@@ -0,0 +1,125 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-educ11",
+      "name": "Educ1#1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.4412481909006623,
+        "pitch": 0.08575384029529864,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4073304254006267,
+          "pitch": 0.05183607479526131,
+          "rotation": 0,
+          "target": "1-bus1guild"
+        },
+        {
+          "yaw": 1.9941508751328776,
+          "pitch": 0.07048353143297703,
+          "rotation": 0,
+          "target": "1-bus1guild"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9653635906205942,
+          "pitch": 0.055487685927392505,
+          "title": "Educ Tent",
+          "text": "Under Renovation"
+        }
+      ]
+    },
+    {
+      "id": "1-bus1guild",
+      "name": "bus1#Guild",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.261496982528886,
+        "pitch": -0.05113441890251025,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3500192593407974,
+          "pitch": 0.05809903771667635,
+          "rotation": 0,
+          "target": "0-educ11"
+        },
+        {
+          "yaw": 1.7181653576251836,
+          "pitch": -0.020500170080360647,
+          "rotation": 0,
+          "target": "0-educ11"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.319263919837635,
+          "pitch": 0.4474480797307798,
+          "title": "Conducive EnvironMent",
+          "text": "EducTent"
+        },
+        {
+          "yaw": 2.566280673530363,
+          "pitch": 0.008413195924550365,
+          "title": "ChemLab1",
+          "text": "For students Practicals"
+        },
+        {
+          "yaw": 0.9211214939050496,
+          "pitch": 0.0158561355458815,
+          "title": "RecycleBin",
+          "text": "For Archieves"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
